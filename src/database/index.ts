ff97c22c02bc9f98a8a375d4b66bd123d7586458
augustocreppe <augusto.creppe@gmail.com>
2021-10-20 23:15:57 -0300

    Add info

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,3 +3,4 @@ import { createConnection } from "typeorm";
 createConnection();
 
 //Criar Migration: npm run typeorm migration:create -- -n CreateNOME
+//Rodar Migration: npm run typeorm migration:run
