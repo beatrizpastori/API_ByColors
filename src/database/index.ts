2b2c674e1cf5ec1ff3b81d6abfbd9fcdb65f73d7
augustocreppe <augusto.creppe@gmail.com>
2021-12-01 11:20:31 -0300

    API - VERSÃO FINAL

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,5 +2,4 @@ import { createConnection } from "typeorm";
 
 createConnection();
 
-//Criar Migration: npm run typeorm migration:create -- -n CreateNOME
 //Rodar Migration: npm run typeorm migration:run
